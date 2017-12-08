9b9eaf57a50700e5b271d59f4f827c29a73164b3
Alice Guan <aliguan726@gmail.com>
2017-12-07 22:20:36 -0500

    Format results from yelp api

--- routes/yelp-router.js
+++ routes/yelp-router.js
@@ -20,15 +20,84 @@ yelp.accessToken(clientId, clientSecret)
 
 //Search for business
 yelpRouter.post('/', (req, res, next) => {
+    var total;
     client.search({
         term: req.body.term,
-        location: req.body.location
+        location: req.body.location,
+        limit: 50,
     }).then(response => {
-        res.send(response.jsonBody.businesses);
+        total = response.jsonBody.total;
+        getBusinesses(total);
     }).catch(e => {
         console.log(e);
     });
+
+    function getBusinesses(total) {
+        var businesses = [];
+
+        var count = 0;
+        for(var i = 0; i < Math.floor(total/50); i++ ) {
+            client.search({
+                term: req.body.term,
+                location: req.body.location,
+                limit: 50,
+                offset: i,
+            }).then(response => {
+                response.jsonBody.businesses.forEach(business => {
+                    switch(business.price) {
+                        case '$':
+                            business.price = 1;
+                            break;
+                        case '$$':
+                            business.price = 2;
+                            break;
+                        case '$$$':
+                            business.price = 3;
+                            break;
+                        case '$$$$':
+                            business.price = 4;
+                            break;
+                        default:
+                            business.price = 2;
+                    }
+                    var item = {
+                        name: business.name,
+                        cost: business.price,
+                        rating: business.rating
+                    }
+                    businesses.push(item);
+                });
+                count++;
+
+                if(count == Math.floor(total/50) ) {
+                    generateEventArrays();
+                }
+            }).catch(e => {
+                console.log(e);
+            });
+        }
+
+        function generateEventArrays() {
+            if(businesses.length > 0) {
+                var itineraries = {
+                    event0: [],
+                    event1: [],
+                    event2: [],
+                    event3: [],
+                    event4: [],
+                    event5: [],
+                    event6: [],
+                }
+                var numberOfItems = Math.floor(businesses.length/7);
+
+            }
+        }
+    }
+
+
+
 });
+
 
 
 module.exports = yelpRouter;
